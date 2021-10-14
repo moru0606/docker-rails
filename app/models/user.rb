9eca1d79367e4e57390a63844bfb979139d723fb
moru0606 <moru150606@gmail.com>
2021-10-15 01:07:46 +0900

    feat:ユーザー登録フォーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,15 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  has_secure_password
+  validates :name,
+  presence: true,
+  uniqueness: true,
+  length: { maximum: 16 },
+  format: { 
+    with: /\A[a-z0-9]+\z/,
+    message: '小文字英数で入力くしてください'
+  }
+  validates :password,
+    length: { minimum: 8 }
 end
